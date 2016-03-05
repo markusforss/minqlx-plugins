ae7ee1e0fde724befe82c5f928ec624e32ecb83e
Markus Forss <markusforss@outlook.com>
2016-03-05 20:06:48 +0200

    Increase delay for playing sound

--- darude.py
+++ darude.py
@@ -1,13 +1,15 @@
 import minqlx
+import time
 
 class darude(minqlx.Plugin):
     def __init__(self):
         super().__init__()
         self.add_hook("map", self.reference_soundpack)
         self.add_command("darude", self.cmd_darude, 5, usage="Darude.. do I need to say more?")
-        self.set_cvar_once("qlx_darudeSoundDelay", "3")
-        self.soundpack_id = "638403084"
+        self.set_cvar("qlx_darudeSoundDelay", "30")
 
+        self.soundpack_id = "638403084"
+        self.last_sound = None
         self.sound_path = "sound/darude/darude.ogg"
 
     def reference_soundpack(self, mapname, factory):
@@ -19,12 +21,12 @@ class darude(minqlx.Plugin):
         self.play_sound()
 
     def play_sound(self):
-        #if not self.last_sound:
-        #    pass
-        #elif time.time() - self.last_sound < self.get_cvar("qlx_darudeSoundDelay", int):
-        #    return
+        if not self.last_sound:
+            pass
+        elif time.time() - self.last_sound < self.get_cvar("qlx_darudeSoundDelay", int):
+            return
 
-        #self.last_sound = time.time()
+        self.last_sound = time.time()
         for p in self.players():
             #if self.db.get_flag(p, "essentials:sounds_enabled", default=True):
             super().play_sound(self.sound_path, p)
